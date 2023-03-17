bb1a40792cf4d3996a057a6aae2b39445e5f37e2
milorhilary <hilaryebube@gmail.com>
2023-03-17 10:22:29 -0700

    Adding Puts.c, Size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int l;
 	float m;
 
-	printf("Size of a char: %lu.\n", (unsigned long)sizeof(i));
-	printf("Size of a int: %lu.\n", (unsigned long)sizeof(j));
-	printf("Size of a long int: %lu.\n", (unsigned long)sizeof(k));
-	printf("Size of a long long int: %lu.\n", (unsigned long)sizeof(l));
-	printf("Size of a float: %lu.\n", (unsigned long)sizeof(m));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(j));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(m));
 
 	return (0);
 }
