40b342200e8365482a6fd121546a3a9b78db75e8
milorhilary <hilaryebube@gmail.com>
2023-04-04 17:26:57 -0700

    Adding 3-strspn.c file, it works!

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,12 +7,12 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, boo1;
+	unsigned int i, j, bool;
 
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		bool = 1;
-		for (j = 0; *(accept + j) != '/0'; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
