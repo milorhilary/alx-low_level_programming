97cc55cb4b7c647f9e84a64d4f927e610778277c
milorhilary <hilaryebube@gmail.com>
2023-04-11 16:04:55 -0700

    Adding create_array file, it works!

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * create_array - creates an array of chars.
  * @size: size of the array.
@@ -9,13 +9,13 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char ch;
+	char *ch;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	ch + malloc(sizeof(c) * size);
+	ch = malloc(sizeof(c) * size);
 
 	if (ch == NULL)
 		return (NULL);
