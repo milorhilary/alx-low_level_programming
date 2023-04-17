2132cbaf5bae3627ef1d8d4fb6659f2638f6f10e
milorhilary <hilaryebube@gmail.com>
2023-04-17 13:42:26 -0700

    Updating header file. it includes the free_dog function

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,9 @@
 #define _DOG_H_
 
 /**
- * dog_m - Typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
-typedef struct dog dog_m;
+typedef struct dog dog_t;
 
 /**
  * struct dog - struct that shores some information of a dog
@@ -24,7 +24,7 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-dog_m *new_dog(char *name, float age, char *owner);
-void free_dog(dog_m *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
